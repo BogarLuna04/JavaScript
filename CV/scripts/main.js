c02bd0acf030a21fd89717d2467d5a6fff09daad
Bogar Luna <bogarluna@MacBook-Pro.local>
2020-08-12 00:11:16 -0500

    Comenzando cv

---
+++ CV/scripts/main.js
@@ -0,0 +1,8 @@
+//const miTitulo = document.querySelector('h1');
+//miTitulo.textContent = '¡Hola mundo!';
+let entrada = '1';
+//if (entrada === '1') {
+//  alert('¡Bienvenido a mi curriculum vitae!');
+//} else {
+//  alert('Bienvenido');
+//}
